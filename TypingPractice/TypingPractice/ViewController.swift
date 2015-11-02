add86e3165cb4a85544a97c28e5fb37a2b2a8518
Susanne Kasahara <kasaharasusanne@gmail.com>
2015-11-02 10:31:36 -0500

    added text field delegate

--- TypingPractice/TypingPractice/ViewController.swift
+++ TypingPractice/TypingPractice/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,19 @@ class ViewController: UIViewController {
 
     }
 
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        return true
+    
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        return true
+        
+        
+    }
+
 
 }
+
 
